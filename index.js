33040948182c9d79a3596200438895460ff0e9f6
Jacob Millner <jacob.millner@outlook.com>
2021-03-05 18:36:42 -0500

    init preact and material ui project

---
+++ index.js
@@ -0,0 +1,14 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import App from './App';
+import theme from './theme';
+
+export default () => (
+	<div id="app">
+		<ThemeProvider theme={theme}>
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</div>
+);
