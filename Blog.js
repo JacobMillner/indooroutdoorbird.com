3e76ac08e3a17544d261a1e13b649c44f813d3b3
Jacob Millner <jacob.millner@outlook.com>
2021-03-05 20:26:27 -0500

    init bird material preact blog

---
+++ Blog.js
@@ -0,0 +1,106 @@
+import { Fragment } from 'preact';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Header from './Header';
+import MainFeaturedPost from './MainFeaturedPost';
+import FeaturedPost from './FeaturedPost';
+import Main from './Main';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+import post1 from './blog-post.1.md';
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const sections = [{ title: 'Birds', url: '#' }];
+
+const mainFeaturedPost = {
+  title: "Outdoor Birds, Hagin' indoors",
+  description:
+    'Have you ever seen an outdoor bird...Indoors? How did he get there? Who is going to get him out? Maybe he likes it here.',
+  image: 'https://i.imgur.com/5Mi3CfA.png',
+  imgText: 'main image description',
+  linkText: 'Send me your Birds…',
+};
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://i.imgur.com/5Mi3CfA.png',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://i.imgur.com/5Mi3CfA.png',
+    imageText: 'Image Text',
+  },
+];
+
+const posts = [post1];
+
+const sidebar = {
+  title: 'About',
+  description:
+    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+  archives: [
+    { title: 'March 2020', url: '#' },
+    { title: 'February 2020', url: '#' },
+    { title: 'January 2020', url: '#' },
+    { title: 'November 1999', url: '#' },
+    { title: 'October 1999', url: '#' },
+    { title: 'September 1999', url: '#' },
+    { title: 'August 1999', url: '#' },
+    { title: 'July 1999', url: '#' },
+    { title: 'June 1999', url: '#' },
+    { title: 'May 1999', url: '#' },
+    { title: 'April 1999', url: '#' },
+  ],
+  social: [
+    { name: 'GitHub', icon: GitHubIcon },
+    { name: 'Twitter', icon: TwitterIcon },
+    { name: 'Facebook', icon: FacebookIcon },
+  ],
+};
+
+export default function Blog() {
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="Outdoor Indoor Birds" sections={sections} />
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Main title="Little Birds Big World" posts={posts} />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
+      </Container>
+      <Footer
+        title="Footer"
+        description="Something here to give the footer a purpose!"
+      />
+    </Fragment>
+  );
+}
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
-import App from './App';
-import theme from './theme';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import Blog from "./Blog";
+import { createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 export default () => (
-	<div id="app">
-		<ThemeProvider theme={theme}>
-			<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-			<CssBaseline />
-			<App />
-		</ThemeProvider>
-	</div>
+  <div id="app">
+    <ThemeProvider theme={theme}>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <CssBaseline />
+      <Blog />
+    </ThemeProvider>
+  </div>
 );
